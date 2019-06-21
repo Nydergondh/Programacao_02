1997e45f62b5ffa427d0a8b455236aa3e0becf40
Vinicius Rosa <viniciusr25@hotmail.com>
2019-06-21 19:06:50 -0300

    Update EnemyZombie.cs

--- Assets/Scripts/Enemies/EnemyZombie.cs
+++ Assets/Scripts/Enemies/EnemyZombie.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyZombie : MonoBehaviour {
-    /*
+    
     private new Collider2D collider;
     private int health = 3;
     private int attackDamage;
@@ -30,5 +30,5 @@ public class EnemyZombie : MonoBehaviour {
             Destroy(transform.gameObject);
         }
     } 
-    */
+    
 }
